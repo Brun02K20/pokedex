395509b01f0a2a056cf509310acae2b6dd96af25
Brun02K23 <bvirinni@gmail.com>
2024-01-04 21:19:00 -0300

    avanzando en la logica de obtencion de datos de la pokeapi

---
+++ src/api/pokemon.js
@@ -0,0 +1,16 @@
+// funciones que se comunican con la api de pokeapi para traer las cosas
+import axios from "axios";
+import { API_HOST } from "../utils/constants.js";
+
+const getPokemonApi = async () => {
+    try {
+        const url = `${API_HOST}/pokemon?limit=20&offset=0`;
+        const response = await axios.get(url)
+        const result = await response.data
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
+export { getPokemonApi }
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,24 @@
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getPokemonApi } from '../api/pokemon.js'
 
 const Pokedex = () => {
+
+
+
+    useEffect(() => {
+        const loadPokemons = async () => {
+            try {
+                const response = await getPokemonApi()
+                console.log(response)
+            } catch (error) {
+                console.error("ERROR AL SOLICITAR: ", error)
+            }
+        }
+        loadPokemons()
+    }, [])
+
     return (
         <SafeAreaView>
             <Text>Pokedex</Text>
